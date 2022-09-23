1978003347420031e1e5a0c62ad1b130760d864b
mcchri <112865223+mcchri@users.noreply.github.com>
2022-09-23 12:36:00 +0100

    Add files via upload

---
+++ as4.js
@@ -0,0 +1,31 @@
+let n = Number(prompt("Enter a number"));
+let a = n % 2;
+if(a==0){
+    console.log("It is even");
+}else{
+    console.log("It is odd");
+}
+
+let x = 6;
+let y = 6;
+
+if (x>y) {
+    console.log("x is bigger " + x);
+}else if (y>x) {
+    console.log("y is bigger " + y);
+}else{
+    console.log("both numbers are the same");
+}
+
+let q = 6;
+let w = 6;
+let e = 6;
+
+if (q>w) {
+    
+    
+}else if{
+    if (condition) {
+        
+    }
+}
