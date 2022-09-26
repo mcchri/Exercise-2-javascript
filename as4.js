93affa472d28abb13db6500e90fdac712af7ec6a
mcchri <112865223+mcchri@users.noreply.github.com>
2022-09-26 15:29:41 +0100

    Update as4.js

--- as4.js
+++ as4.js
@@ -1,3 +1,4 @@
+// To check of which two numbers is even or odd
 let n = Number(prompt("Enter a number"));
 let a = n % 2;
 if(a==0){
@@ -5,7 +6,7 @@ if(a==0){
 }else{
     console.log("It is odd");
 }
-
+// To check of which two numbers is the biggest
 let x = 6;
 let y = 6;
 
@@ -16,16 +17,41 @@ if (x>y) {
 }else{
     console.log("both numbers are the same");
 }
-
-let q = 6;
+// To check of which three numbers is the biggest
+let q = 4;
 let w = 6;
-let e = 6;
+let e = 4;
 
 if (q>w) {
-    
-    
-}else if{
-    if (condition) {
-        
+    if (q>e) {
+        console.log("q is the biggest");
+    }else{
+        console.log("e is the biggest");
     }
+    
+}else if(w>e){
+    console.log("w is the biggest");
 }
+//Triangle if it is scalene, equalateral or isosceles
+
+line1 = 4;
+line2 = 5;
+line3 = 6;
+if(line1 == line2 && line1 == line3){
+    console.log("It is a equalateral triangle");
+}else if(line1 == line2 || line1 == line3 || line2 == line3){
+    console.log("It is a isosceles triangle");
+}else{
+    console.log("It is a scalene triangle");
+}
+
+//arthemitic operations
+
+num1 = Number(prompt("Please enter a value "));
+num2 = Number(prompt("Please enter a second value "));
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
